5ae12ae00c30a3201c3b9ce9b37c5a66ceb126e5
Nino <nino.rameau@hotmail.com>
2024-12-06 15:39:45 +0100

    maj header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-// menu burger
 
-const buttonAction = document.querySelector(".menu");
-const navMain = document.querySelector(".header-sommaire");
+function toggleMobileNav() {
+  const buttonAction = document.querySelector(".menu");
+  const navMain = document.querySelector(".header-sommaire");
 
-if (buttonAction) {
-  buttonAction.addEventListener("click", function () {
-    navMain.classList.toggle("menu-open");
-  });
+  if (buttonAction) {
+    buttonAction.addEventListener("click", function () {
+      navMain.classList.toggle("menu-open");
+    });
+  }
 }
+
+function toggleDarkMode() {
+  console.log("Dark mode function");
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  // Charge la gestion menu mobile
+  toggleMobileNav();
+  //Charger la gestion du darkmod
+  toggleDarkMode();
+});
