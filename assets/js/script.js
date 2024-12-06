270fd3e50fbea4873421fe42081ea90e9639e750
Nino <nino.rameau@hotmail.com>
2024-12-06 13:25:33 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,10 @@
 // menu burger
 
-const buttonAction = document.querySelector(".menuToggle");
-const navMain = document.querySelector(".navMain");
+const buttonAction = document.querySelector(".menu");
+const navMain = document.querySelector(".header-sommaire");
 
 if (buttonAction) {
   buttonAction.addEventListener("click", function () {
-    navMain.classList.toggle("MenuBurger");
+    navMain.classList.toggle("menu-open");
   });
-}
-
-// mode clair / sombre
-
-// recuperer les elt
-const myButton = document.querySelector("#toogleBackground");
-const body = document.querySelector("body");
-
-// verifie btn
-if (myButton) {
-  // cree fonction pour changer background
-  function toggleBackground() {
-    body.classList.toggle("ClairSombre");
-  }
-
-  //ajoute un ecouteur d'evenement
-  myButton.addEventListener("click", toggleBackground);
 }
