bf29da35446db607575d6b1b3fb8aed73f067444
Nino <nino.rameau@hotmail.com>
2024-12-19 20:22:53 +0100

    maj des carrousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,9 +31,69 @@ function toggleDarkMode() {
   myButton.addEventListener("click", toggleBackground);
 }
 
+function carrousel() {
+  "use strict";
+
+  // Sélectionner tous les carrousels
+  const carrousels = document.querySelectorAll(".container-carrousel");
+
+  // Initialiser chaque carrousel
+  carrousels.forEach((carousel) => {
+    const diapos = carousel.querySelectorAll(".diapo");
+    const conteneurPoints = carousel.querySelector(".points-navigation");
+    const boutonPrecedent = carousel.querySelector(".precedent");
+    const boutonSuivant = carousel.querySelector(".suivant");
+    let diaposActuelle = 0;
+
+    // Créer les points de navigation
+    diapos.forEach(() => {
+      const point = document.createElement("span");
+      point.classList.add("point");
+      conteneurPoints.appendChild(point);
+    });
+
+    const points = conteneurPoints.querySelectorAll(".point");
+
+    function afficherDiapo(index) {
+      diaposActuelle = index;
+      diapos.forEach(
+        (diapo) =>
+          (diapo.style.transform = `translateX(-${diaposActuelle * 100}%)`)
+      );
+      points.forEach((point, i) => {
+        point.classList.toggle("actif", i === diaposActuelle);
+      });
+    }
+
+    boutonSuivant.addEventListener("click", () => {
+      if (diaposActuelle < diapos.length - 1) {
+        afficherDiapo(diaposActuelle + 1);
+      } else {
+        afficherDiapo(0);
+      }
+    });
+
+    boutonPrecedent.addEventListener("click", () => {
+      if (diaposActuelle > 0) {
+        afficherDiapo(diaposActuelle - 1);
+      } else {
+        afficherDiapo(diapos.length - 1);
+      }
+    });
+
+    points.forEach((point, index) => {
+      point.addEventListener("click", () => afficherDiapo(index));
+    });
+
+    afficherDiapo(0);
+  });
+}
+
 window.addEventListener("DOMContentLoaded", function () {
   // Charge la gestion menu mobile
   toggleMobileNav();
   //Charger la gestion du darkmod
   toggleDarkMode();
+  // carrousel
+  carrousel();
 });
